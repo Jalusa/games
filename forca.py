207cf8dbcc7076568f9d622243e41ef3c454fcfc
Jalusa <jalusaferrari@gmail.com>
2021-12-02 14:19:49 -0300

    Update forca.py
    Adicionado a condição para ganhar e perder o jogo.

--- forca.py
+++ forca.py
@@ -4,30 +4,44 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
 
     letras_certas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
+    erros = 0
+
     print("Dica: {} letras! ".format(len(letras_certas)))
+
     while(not enforcou and not acertou):
+
         chute = input("Chute uma letra: ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}.".format(letra,index))
-                letras_certas[index] = letra
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    #print("Encontrei a letra {} na posição {}.".format(letra,index))
+                    letras_certas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
-            index = index + 1
-        #else:
-        #    print("A letra {} não existe na palavra.".format(chute))
+        if "_" not in letras_certas:
+            print("Você acertou! A palavra secreta é {}.".format(palavra_secreta))
+            break
+
+        enforcou = erros == 6
+        if(enforcou):
+            print("Você perdeu. Suas chances acabaram!")
+            break
+
         print("A palavra é {}.".format(letras_certas))
-
+        chances = 6 - erros
         letras_faltando = str(letras_certas.count('_'))
-        print('Ainda faltam acertar {} letras'.format(letras_faltando))
+        print('Ainda faltam acertar {} letras. Você tem {} chances de errar!'.format(letras_faltando,chances))
 
     print("Fim do jogo")
 
