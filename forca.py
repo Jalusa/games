79adcfada9dcdc57221d9daf9c5e1823193eda05
Jalusa <jalusaferrari@gmail.com>
2021-12-04 08:23:20 -0300

    Leitura de uma palavra do arquivo palavra.txt

--- forca.py
+++ forca.py
@@ -1,32 +1,39 @@
+import random
 
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    arquivo = open("palavras.txt","r")
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.apped(linha)
 
+    arquivo.close()
 
-    palavra_secreta = "banana".upper()
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
 
     letras_certas = ["_" for letras in palavra_secreta]
-    
+
     enforcou = False
     acertou = False
     erros = 0
 
     print("Dica: {} letras! ".format(len(letras_certas)))
 
-    while(not enforcou and not acertou):
+    while (not enforcou and not acertou):
 
         chute = input("Chute uma letra: ")
         chute = chute.strip().upper()
 
-        if(chute in palavra_secreta):
+        if (chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
-                if(chute == letra):
-                    #print("Encontrei a letra {} na posição {}.".format(letra,index))
+                if (chute == letra):
+                    # print("Encontrei a letra {} na posição {}.".format(letra,index))
                     letras_certas[index] = letra
                 index += 1
         else:
@@ -37,16 +44,17 @@ def jogar():
             break
 
         enforcou = erros == 6
-        if(enforcou):
+        if (enforcou):
             print("Você perdeu. Suas chances acabaram!")
             break
 
         print("A palavra é {}.".format(letras_certas))
         chances = 6 - erros
         letras_faltando = str(letras_certas.count('_'))
-        print('Ainda faltam acertar {} letras. Você tem {} chances de errar!'.format(letras_faltando,chances))
+        print('Ainda faltam acertar {} letras. Você tem {} chances de errar!'.format(letras_faltando, chances))
 
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     jogar()
